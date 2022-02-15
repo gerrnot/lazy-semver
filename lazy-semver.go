7726177e3813fb42e05ea95adcee4f6d2771a2a0
Gernot Feichter <gernot.feichter@bearingpoint.com>
2022-02-15 17:24:51 +0100

    feat: use github.com/antchfx/xmlquery

--- lazy-semver.go
+++ lazy-semver.go
@@ -4,14 +4,13 @@ import (
 	"flag"
 	"fmt"
 	"github.com/antchfx/xmlquery"
-	"github.com/antchfx/xpath"
 	"io/ioutil"
 	"strings"
 )
 
 func main() {
 	filePath, xPathPattern, baseVersionRegex := handleFlags()
-	baseVersion := getBaseVersion(*filePath, *xPathPattern)
+	baseVersion := getBaseVersion(*filePath, *xPathPattern) // base version is major.minor version and looks like 1.0
 
 	fmt.Printf("Args were filePath: %s, %s, %s\n", *filePath, *xPathPattern, *baseVersionRegex)
 	fmt.Printf("Parsed base version is: %s\n", baseVersion)
@@ -28,28 +27,26 @@ func getBaseVersion(filePath string, xPathPattern string) string {
 	// process
 	if xPathPattern == "" {
 		return fileContentString
-	} else {
-		expr, err := xpath.Compile(xPathPattern)
-		if err != nil {
-			panic(err)
-		}
+	} else if strings.HasSuffix(strings.ToLower(filePath), ".xml") {
 		doc, err := xmlquery.Parse(strings.NewReader(fileContentString))
 		if err != nil {
 			panic(err)
 		}
-		baseVersionNode := expr.Evaluate(xmlquery.CreateXPathNavigator(doc)).(*xpath.NodeIterator)
-		fmt.Printf("baseVersionString=%s", baseVersionNode.Current().Value())
+		baseVersionNode := xmlquery.FindOne(doc, xPathPattern)
+		return baseVersionNode.InnerText()
+	} else {
+		panic("You passed neither an xml file, nor a plaintext with an empty argument xPathPattern. " +
+			"These are to only types currently supported.")
 	}
-	panic("Error: ")
-	return "" /* This line will not be executed, but the go compiler complains otherwise */
 }
 
 func handleFlags() (*string, *string, *string) {
-	filePath := flag.String("filePath", "no filePath provided", "filePath to a filename including the "+
-		"extensions, of which the base version should be read")
+	filePath := flag.String("filePath", "no filePath provided", "filePath to a filename "+
+		"including the extensions, of which the base version should be read")
 	xPathPattern := flag.String("xPathPattern", "", "XPath Pattern to select the version. If "+
 		"empty, the whole content of the file will be used as base version string.")
-	baseVersionRegex := flag.String("baseVersionRegex", "\\d+.\\d+", "the regex used to parse the base version (that is the major.minor version part)")
+	baseVersionRegex := flag.String("baseVersionRegex", "\\d+.\\d+",
+		"the regex used to parse the base version (that is the major.minor version part)")
 	flag.Parse()
 	return filePath, xPathPattern, baseVersionRegex
 }
